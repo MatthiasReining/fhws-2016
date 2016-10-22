caaea6f963816bc9856659b637758f6cc74e82b7
Matthias Reining <matthias.reining@outlook.com>
2016-10-22 12:16:49 +0200

    add unit test

---
+++ src/test/java/de/fhws/app/business/boundary/calculation/CalculationTest.java
@@ -0,0 +1,32 @@
+package de.fhws.app.business.boundary.calculation;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class CalculationTest {
+	
+	Calculation cut;
+	
+	@Before
+	public void init() {
+		cut = new Calculation();
+	}
+
+	
+	@Test
+	public void shouldCalculate() {
+		
+		double result = cut.calc(1); 
+		Assert.assertEquals(42.0, result, 0.001);
+		
+	}
+	
+	@Test
+	public void shouldCalculate2() {		
+		double result = cut.calc(2); 
+		Assert.assertEquals(21.0, result, 0.001);
+		
+	}
+}
