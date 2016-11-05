efcf6c511518fe17d5c6ed166320c93d5a05c3af
Matthias Reining <matthias.reining@outlook.com>
2016-11-05 16:40:38 +0100

    EJB WebService (SOAP)

--- src/main/java/de/fhws/app/business/calculation/boundary/ComplexCalculationBean.java
+++ src/main/java/de/fhws/app/business/calculation/boundary/ComplexCalculationBean.java
@@ -2,14 +2,26 @@ package de.fhws.app.business.calculation.boundary;
 
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.jws.WebService;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import de.fhws.app.business.log.entity.LogData;
 
 @Stateless
 @Remote(ComplexCalculationBeanRemote.class)
+@WebService
 public class ComplexCalculationBean implements ComplexCalculationBeanLocal, ComplexCalculationBeanRemote {
+	@PersistenceContext
+	EntityManager em;
 
 	@Override
 	public int calc(int in) {
 		return 42 * in;
 	}
 
+	public LogData getEvent(long id) {
+		
+		return em.find(LogData.class, id);
+	}
 }
--- src/main/java/de/fhws/app/business/calculation/boundary/ComplexCalculationBeanRemote.java
+++ src/main/java/de/fhws/app/business/calculation/boundary/ComplexCalculationBeanRemote.java
@@ -1,7 +1,11 @@
 package de.fhws.app.business.calculation.boundary;
 
+import de.fhws.app.business.log.entity.LogData;
+
 public interface ComplexCalculationBeanRemote {
 
 	public int calc(int in);
+	
+	public LogData getEvent(long id);
 
 }
