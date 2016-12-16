56a5d6793d32eae061eac470443eb9cb131aa550
Matthias Reining <matthias.reining@outlook.com>
2016-12-16 13:33:03 +0100

    inject chain

---
+++ src/main/java/de/fhws/app/presentation/showcase/DataContainer.java
@@ -0,0 +1,26 @@
+package de.fhws.app.presentation.showcase;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+
+public class DataContainer implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String data;
+	
+	private static int counter = 0;
+	
+	@PostConstruct
+	public void init() {
+		counter++;
+		data = counter + " Message ";
+	}
+	
+	public String getData() {
+		return data;
+	}
+
+
+}
--- src/main/java/de/fhws/app/presentation/showcase/Message.java
+++ src/main/java/de/fhws/app/presentation/showcase/Message.java
@@ -3,6 +3,7 @@ package de.fhws.app.presentation.showcase;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 //@RequestScoped //see different behaviour
 //@Dependent //see different behaviour
@@ -14,10 +15,13 @@ public class Message implements Serializable {
 	
 	private static int counter = 0;
 	
+	@Inject DataContainer dc;
+	
 	@PostConstruct
 	public void init() {
 		counter++;
 		message = counter + " Message ";
+		System.out.println( dc.getData()) ;
 	}
 
 	public String getMessage() {
